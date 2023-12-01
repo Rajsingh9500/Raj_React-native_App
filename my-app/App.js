2a74a988ac455c919540aaddff3bfd33b6c26d2f
Rajsingh9500 <rajthakur9500@gmail.com>
2023-12-01 23:50:27 +0530

    raj

---
+++ my-app/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hey man vaibhav cc</Text>
+      <Text>madarchod kashish chod</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
