46ed431b96a889f832a72e2c69a8b3a2229c2851
Rajsingh9500 <rajthakur9500@gmail.com>
2023-12-05 17:27:17 +0530

    by raj

--- my-app/App.js
+++ my-app/App.js
@@ -1,30 +1,120 @@
-
-import {Dimensions, ImageBackground, Text, View } from 'react-native';
-import React from "react";
+import { View, Text, ScrollView, Image, Linking } from 'react-native'
+import React from 'react'
 import styles from "./style";
-/*  const a = Dimensions.get('screen').height; */
-/* alert(`width  = ${Dimensions.get('screen').fontScale}`) */
+export default function () {
+return (
 
-export default function App() {
-  return (
-   <> 
-   <ImageBackground  style ={styles.img} source={require('./assets/images/pic1.jpg')}>
-   <View style = {styles.a}>
-   <Text>Discover Mindfull hiking</Text>
-  </View>
-  <ImageBackground style = {styles.b} source={require('./assets/images/pic2.jpg')}>
-  <View style = {styles.b  }>
-    <Text>
-      hey coder
-    </Text>
+<>
+<View>
+<Text style = {{margin:50}} onPress={()=> {Linking.openURL('https://google.com')}}>
+  Google 
+</Text>
+</View>
+
+
+<ScrollView showsVerticalScrollIndicator = {false} horizontal = {false}>
+   {/*  <View style = {styles.a} >
+      <View style = {styles.main}>
+        <Text style = {{fontSize:24,
+        color:'green', transform:[{rotate:'30deg'},{scale:1},{translateY:40}]}
+        }>Hello</Text>
+      </View>
+      <View style = {[styles.main , styles.a1]}>
+        <Text  style = {{fontSize:24,
+        color:'purple', transform:[{rotate:'45deg'}]} }>
+          Hello</Text>
+      </View>
+      <View style = {styles.main}>
+        <Text style = {styles.txt}>Hello</Text>
+      </View>
+      <View style = {[styles.main , styles.a1]}>
+        <Text>Hello</Text>
+      </View>
+        
+        
+    </View> */}
+    <View style = {styles.srl1}>
+    <View style = {styles.img1 }>
+    <Image  style = {styles.img1} source={require('./assets/images/pic1.jpg')}>
+     
+
+    </Image>
     </View>
-  </ImageBackground>
-    </ImageBackground>
-    </>
-  
-  );
-}  
+    <View style = {styles.img1 }>
+    <Image  style = {styles.img1} source={require('./assets/images/pic1.jpg')}>
+     
 
+    </Image>
+    </View>
+    <View style = {styles.img1 }>
+    <Image  style = {styles.img1} source={require('./assets/images/pic1.jpg')}>
+     
+
+    </Image>
+    </View>
+    <View style = {styles.img1 }>
+    <Image  style = {styles.img1} source={require('./assets/images/pic1.jpg')}>
+     
+
+    </Image>
+    </View>
+    </View>
+    </ScrollView>
+
+   
+    <ScrollView showsHorizontalScrollIndicator = {false} horizontal= {true}>
+   {/*  <View style = {styles.a} >
+      <View style = {styles.main}>
+        <Text style = {{fontSize:24,
+        color:'green', transform:[{rotate:'30deg'},{scale:1},{translateY:40}]}
+        }>Hello</Text>
+      </View>
+      <View style = {[styles.main , styles.a1]}>
+        <Text  style = {{fontSize:24,
+        color:'purple', transform:[{rotate:'45deg'}]} }>
+          Hello</Text>
+      </View>
+      <View style = {styles.main}>
+        <Text style = {styles.txt}>Hello</Text>
+      </View>
+      <View style = {[styles.main , styles.a1]}>
+        <Text>Hello</Text>
+      </View>
+        
+        
+    </View> */}
+    <View style = {styles.srl}>
+    <View style = {styles.img }>
+    <Image  style = {styles.img} source={require('./assets/images/pic1.jpg')}>
+     
+
+    </Image>
+    </View>
+    <View style = {styles.img }>
+    <Image  style = {styles.img} source={require('./assets/images/pic1.jpg')}>
+     
+
+    </Image>
+    </View>
+    <View style = {styles.img }>
+    <Image  style = {styles.img} source={require('./assets/images/pic1.jpg')}>
+     
+
+    </Image>
+    </View>
+    <View style = {styles.img }>
+    <Image  style = {styles.img} source={require('./assets/images/pic1.jpg')}>
+     
+
+    </Image>
+    </View>
+    </View>
+    </ScrollView>
+   
+    </>
+    
+  )
+}
 
 
 
--- my-app/style.js
+++ my-app/style.js
@@ -1,31 +1,83 @@
+import { StyleSheet, Stylesheet } from 'react-native'
+  const styles =   StyleSheet.create({
+/*   a:{
 
+    flexDirection:'row',
+  
 
-  import { StyleSheet } from "react-native";
-  const styles = StyleSheet.create({
-   img:{
-    flex:1,
-   },
-    a:{
-     flex:1,
-      fontSize:'30px',
-      padding:'10%',
-      width:'80%',
-      marginLeft:20,
-      marginTop:'30%',
-      fontWeight:'bold',
-      
-     
-      height:'50%',
-    },
+  },
+    main:{
+      flex:1,
+   
 
-    b:{
-     
-      fontStyle:'italic',
+    margin:10,
+    backgroundColor : 'gray',
+    height:'100%',
+    width: '50%',
+    justifyContent:'center',
+    alignItems:'center',
 
-      height:'60%',
-      fontSize:50,
-      
     
-    }
-  })
-  export default styles
+
+  },
+    a1:{
+    
+    backgroundColor :'red',
+  
+    
+    
+  },
+  txt:{
+    backgroundColor:'red',
+    transform : [
+    
+      {  rotate:'48deg'},
+        {translateX:40},
+        {scale : .5},
+      
+    ]
+  },
+ */
+srl:{
+  flex:1,
+  flexDirection: 'row',
+  height : '100%'
+},
+srl1:{
+  flex:1,
+  flexDirection: 'column',
+  height : '90%'
+},
+  img:{
+    height:200,
+    width:200,
+    borderRadius:100,
+    margin:20,
+    
+    
+   
+  
+    
+    justifyContent:'center',
+    alignItems:'center',
+/*    elevation : 5, */
+   
+  },
+  img1:{
+    height:300,
+    width:300,
+    borderRadius:150,
+    margin:20,
+    
+    
+   
+  
+    
+    justifyContent:'center',
+    alignItems:'center',
+/*    elevation : 5, */
+   
+  }
+})
+
+export default styles
